4a85fed847151174d379a4d14a9cac84170b2bee
Ruofeng Lei <ruofenglei@outlook.com>
2021-02-18 11:11:15 +0800

    build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+mkdir -p bin
+cd ~/bin
+wget https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_0.80.0_Linux-64bit.tar.gz
+tar -xvzf ./hugo_0.80.0_Linux-64bit.tar.gz
+
+./hugo version
+cd ..
+
+./bin/hugo -D
