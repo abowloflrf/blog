caead2874c0e1a6668c33a81f83e6b4dcf5cb3a6
Ruofeng Lei <ruofenglei@outlook.com>
2021-07-10 13:55:21 +0800

    fix build
    Signed-off-by: Ruofeng Lei <ruofenglei@outlook.com>

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ set -ex
 mkdir -p bin
 cd bin
 
-HUGO_VERSION=0.85.0
-wget https://github.com/gohugoio/hugo/releases/download/v$(HUGO_VERSION)/hugo_$(HUGO_VERSION)_Linux-64bit.tar.gz -O hugo.tar.gz
+HUGO_VERSION="0.85.0"
+
+wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -O hugo.tar.gz
 tar -xvzf ./hugo.tar.gz
 
 ./hugo version
